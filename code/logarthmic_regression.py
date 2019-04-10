caac381781240bb40bc8e6ff30444c0755314169
Colin Sauze <cos@aber.ac.uk>
2019-04-10 01:33:06 +0100

    cleaned gdp data some more removed some very high income countries that were acting as outliers and messing up graphs fixed graphing and nan handling for log and non-logdata

--- code/logarthmic_regression.py
+++ code/logarthmic_regression.py
@@ -31,8 +31,6 @@ def least_squares(data):
     for i in range(0, n):
         x = data[0][i]
         y = data[1][i]
-        if x == math.NaN or y == math.NaN:
-            continue
         x_sum = x_sum + x
         y_sum = y_sum + y
         x_sq_sum = x_sq_sum + (x**2)
@@ -54,6 +52,7 @@ def measure_error(data1, data2):
     '''Measure the RMS error between data1 and data2'''
     assert len(data1) == len(data2)
     err_total = 0
+    items_counted = 0 
     for i in range(0, len(data1)):
         err_total = err_total + (data1[i] - data2[i]) ** 2
 
@@ -61,7 +60,7 @@ def measure_error(data1, data2):
     return err
 
 
-def make_graph(x_data, y_data):
+def make_graph(x_data, y_data, y_model):
 
     # rotate the x labels to fit better
     plt.xticks(rotation=90)
@@ -72,15 +71,32 @@ def make_graph(x_data, y_data):
     max_y = math.ceil(max(y_data))
 
     # evenly space y axis, interval of 1, between the min and max life exp
-    plt.yticks(np.linspace(min_y, max_y,
-                           (max_y - min_y)+1))
+    # set to 1000 when using linearised data
+    yticks = list(range(0,max_y,1))
+    # uncomment for linearised version
+    #yticks = list(range(0,max_y,5000))
+    labels = []
+    for y in yticks:
+        #comment out if using linearisd data
+        labels.append(int(math.exp(y)))
+        #uncomment if using linearised data
+        #labels.append(int(y))
+
+
+    print(yticks,labels)
+    plt.yticks(yticks,labels)
+#                           (max_y - min_y)+1))
 
     plt.xlim(min(x_data), max(x_data))
     plt.ylim(min_y, max_y)
 
+    # label axes
+    plt.ylabel("GDP (US $)")
+    plt.xlabel("Life Expectancy")
+
     # draw the line
-    plt.plot(x_data, y_data, label="Original Data")
-    #plt.plot(x_data, y_model, label="Line of best fit")
+    plt.scatter(x_data, y_data, label="Original Data")
+    plt.plot(x_data, y_model, c='orange', label="Line of best fit")
 
     plt.grid()
 
@@ -105,18 +121,44 @@ def read_data(gdp_file,life_expectancy_file, year):
     data = []
     for country in life_expectancy.index:
         if country in gdp.index:
-            data.append((country, life_expectancy[country], gdp[country]))
+            # exclude any country where either (or both) pieces of data are unknown
+            if math.isnan(life_expectancy[country]) == False and math.isnan(gdp[country]) == False:
+                data.append((country, life_expectancy[country], gdp[country]))
+            else:
+                print("Excluding ",country,",NaN in data (life_exp = ",life_expectancy[country],"gdp=",gdp[country],")")
         else:
             print(country,"is not in the GDP country data")
 
     combined = pd.DataFrame.from_records(data,columns=("Country","Life Expectancy","GDP"))
     combined = combined.set_index("Country")
+    # we'll need sorted data for graphing properly later on
+    combined = combined.sort_values("Life Expectancy")
     return combined
 
-data = process_gdp_data("../data/worldbank-gdp.csv", "../data/gapminder-life-expectancy.csv", "2016")
+def process_data(gdp_file, life_expectancy_file, year):
+    data = read_data(gdp_file, life_expectancy_file, year)
 
-gdp_log = data["GDP"].apply(math.log).tolist()
-life_exp = data["Life Expectancy"].tolist()
+    gdp = data["GDP"].tolist()
+    gdp_log = data["GDP"].apply(math.log).tolist()
+    life_exp = data["Life Expectancy"].tolist()
 
-#least_squares([life_exp,gdp_log])
-make_graph(life_exp,gdp_log)
+    m,c = least_squares([life_exp,gdp_log])
+
+    y_model = []
+    for x in life_exp:
+        y_log = m *x +c
+        y = math.exp(y_log)
+
+        y_model.append(y_log)
+
+    print("done making y_model")
+    print("life_exp:",life_exp)
+    print("gdp:",gdp)
+    print("y_model:",y_model)
+    make_graph(life_exp, gdp_log, y_model)
+    print("made graph")
+
+    err = measure_error(y_model, gdp)
+    print("error=",err)
+
+process_data("../data/worldbank-gdp.csv", "../data/gapminder-life-expectancy.csv", "1980")
