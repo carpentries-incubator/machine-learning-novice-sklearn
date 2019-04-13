311b6c0a2b15715ac84aba4b2a293beabd78cd45
Colin Sauze <cos@aber.ac.uk>
2019-04-13 03:09:01 +0100

    3d graphs for clustering

--- code/sklearn-spectral_clustering.py
+++ code/sklearn-spectral_clustering.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
+import matplotlib
+import mpl_toolkits.mplot3d as plt3d
 import sklearn.cluster as skl_cluster
 import sklearn.datasets as skl_data
+import numpy as np
+
+
+def plot_3d_separation(circles,labels):
+
+    #create two empty arrays for the points inside a list
+    circles2 = [np.zeros([200,3]),np.zeros([200,3])]
+
+    circle_indicies = [0,0]
+    for i in range(0,400):
+        x = circles[i][0]
+        y = circles[i][1]
+        label = labels[i]
+        j = circle_indicies[label]
+        circles2[label][j][0] = x
+        circles2[label][j][1] = y
+        circles2[label][j][2] = label
+        circle_indicies[label] = circle_indicies[label] + 1
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.scatter(circles2[0][:, 0],circles2[0][:, 1],circles2[0][:, 2],c=matplotlib.cm.flag(0))
+    ax.scatter(circles2[1][:, 0],circles2[1][:, 1],circles2[1][:, 2],c=matplotlib.cm.flag(255))
+
+    plt.show()
+    #plt3d.Axes3D.plot(x,y,labels)
+
 
 
 def cluster_circles():
@@ -19,34 +48,17 @@ def cluster_circles():
     clusters = Kmean.predict(circles)
 
     # plot the data, colouring it by cluster
-   # plt.scatter(circles[:, 0], circles[:, 1], s=5, c=clusters)
+    plt.scatter(circles[:, 0], circles[:, 1], s=15, linewidth=0.1, c=clusters,cmap='flag')
+    plt.show()
 
     model = skl_cluster.SpectralClustering(n_clusters=2,
                                            affinity='nearest_neighbors',
                                            assign_labels='kmeans')
 
     labels = model.fit_predict(circles)
-#    plt.scatter(circles[:, 0], circles[:, 1], s=5, c=labels)
-
-    import numpy as np
-
-
-    #create two empty arrays for the points inside a list
-    circles2 = [np.zeros([200,2]),np.zeros([200,2])]
-
-    circle_indicies = [0,0]
-    for i in range(0,400):
-        x = circles[i][0]
-        y = circles[i][1]
-        label = labels[i]
-        j = circle_indicies[label]
-        circles2[label][j][0] = x
-        circles2[label][j][1] = y
-        circle_indicies[label] = circle_indicies[label] + 1
-
-
-    print(circles2[0])
-
-    plt.scatter(circles2[1][:, 0], circles2[1][:, 1], s=5)
+    print(labels)
+    plt.scatter(circles[:, 0], circles[:, 1], s=15, linewidth=0, c=labels, cmap='flag')
+    plt.show()
+    plot_3d_separation(circles,labels)
 
 cluster_circles()
